5ca9969c2025203ac3a3517fd0cd28781febcb8b
Tobias Schmitt <tschmittldk@gmail.com>
2024-03-16 09:00:33 +0100

    nvim and catppuccino

--- copyfiles.sh
+++ copyfiles.sh
@@ -8,17 +8,27 @@ SCRIPTPATH=~/.dotfiles
 echo "linking files..."
 echo "- vimrc"
 ln -sf $SCRIPTPATH/vimrc ~/.vimrc
+
 echo "- gitconfig"
 ln -sf $SCRIPTPATH/gitconfig ~/.gitconfig
+
 echo "- bashrc"
 echo "source $HOME/.dotfiles/bashrc" >> $HOME/.bashrc
+
 echo "- zshrc"
 echo "source $HOME/.dotfiles/zshrc" >> $HOME/.zshrc
 echo "- tmux.conf"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -sf $SCRIPTPATH/tmux.conf ~/.tmux.conf
+
 echo "- vim/"
 ln -sf $SCRIPTPATH/vim ~/.vim
+
+echo "- neovim/"
+ln -sf $SCRIPTPATH/nvim ~/.config/nvim
+
 echo "- creating vim_local"
 touch $SCRIPTPATH/vim_local
+
 echo "- assume unchanged"
 bash assume-unchanged.sh
